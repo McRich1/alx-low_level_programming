4a6d741682e51fce1956ea4be5c04e355aabfeea
McRich1 <mcrich.md@gmail.com>
2022-07-28 14:47:29 -0700

    mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,15 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _main_h_
+#define _main_h_
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
