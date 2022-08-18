2cc6e029130b05898958a5b739175b290ebce900
McRich1 <mcrich.md@gmail.com>
2022-08-18 08:58:42 -0700

    done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,9 @@
-#ifndef LIST_H
-#define LIST_H
-#include<stdio.h>
-#include<string.h>
+#ifndef _MAIN_
+#define _MAIN_
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-void print_b_helper(unsigned long int, unsigned long int);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
@@ -14,3 +11,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
 #endif
+
