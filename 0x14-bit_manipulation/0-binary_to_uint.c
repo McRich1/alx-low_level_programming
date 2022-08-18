e83437c9347f8eddcaca1890bb5d0782a4b2e390
McRich1 <mcrich.md@gmail.com>
2022-08-18 08:06:08 -0700

    tried fixing

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,23 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_int - converts a binary to int
+ * @s: pointer to binary chars
+ * Return: int value or zero
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *s)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int res = 0, i = 0;
 
-	if (!b)
+	if (!s)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (; s[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (s[i] < '0' || s[i] > '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		res = res << 1;
+		res |= (s[i] - '0');
 	}
 
-	return (ui);
+	return (res);
 }
